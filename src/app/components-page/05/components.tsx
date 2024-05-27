111e68bd542720a8ccc3ac19f4a7e1de89045b16
kanhaiya0999 <92316307+kanhaiya0999@users.noreply.github.com>
2024-05-27 15:11:33 +0530

    fgdfgdf

--- src/app/components-page/05/components.tsx
+++ src/app/components-page/05/components.tsx
@@ -3,8 +3,47 @@ import { useGSAP } from "@gsap/react";
 
 const Components = () => {
   const timeline = gsap.timeline({ paused: true });
-  useGSAP(() => {});
-  return <div>hello</div>;
+
+  useGSAP(() => {
+    timeline.to(".india-container", {
+      duration: 0.5,
+      scale: 1.5,
+      ease: "power1",
+    });
+    timeline.to(
+      ".indiatext",
+      {
+        color: "#FF9900", // update text color to Indian flag color
+        textShadow: "0px 0px 20px white",
+      },
+      "<"
+    );
+  });
+
+  return (
+    <div
+      className="india-container p-5 "
+      onMouseEnter={() => {
+        timeline.play();
+        console.log("hovered");
+      }}
+      onMouseLeave={() => {
+        timeline.reverse();
+        console.log("unhovered");
+      }}
+    >
+      <span className="text-7xl uppercase font-bold indiatext">I</span>{" "}
+      {/* change text color to Indian flag color indiatext */}
+      <span className="text-7xl uppercase font-bold indiatext">N</span>{" "}
+      {/* change text color to Indian flag color indiatext */}
+      <span className="text-7xl uppercase font-bold indiatext">D</span>{" "}
+      {/* change text color to Indian flag color indiatext */}
+      <span className="text-7xl uppercase font-bold indiatext">I</span>{" "}
+      {/* change text color to Indian flag color indiatext */}
+      <span className="text-7xl uppercase font-bold indiatext">A</span>{" "}
+      {/* change text color to Indian flag color */}
+    </div>
+  );
 };
 
 export default Components;
--- src/app/components-page/06/components.tsx
+++ src/app/components-page/06/components.tsx
@@ -3,8 +3,50 @@ import { useGSAP } from "@gsap/react";
 
 const Components = () => {
   const timeline = gsap.timeline({ paused: true });
-  useGSAP(() => {});
-  return <div>hello</div>;
+
+  useGSAP(() => {
+    timeline.to(".indiatext", {
+      duration: 0.5,
+      fontSize: "7rem",
+      ease: "power1",
+      stagger: 0.2,
+    });
+
+    timeline.to(
+      ".indiatext",
+      {
+        color: "#FF9900", // update text color to Indian flag color
+        textShadow: "0px 0px 20px white",
+        stagger: 0.2,
+      },
+      "<"
+    );
+  });
+
+  return (
+    <div
+      className="india-container p-5  justify-center"
+      onMouseEnter={() => {
+        timeline.play();
+        console.log("hovered");
+      }}
+      onMouseLeave={() => {
+        timeline.reverse();
+        console.log("unhovered");
+      }}
+    >
+      <span className="text-7xl uppercase font-bold indiatext">I</span>{" "}
+      {/* change text color to Indian flag color indiatext */}
+      <span className="text-7xl uppercase font-bold indiatext">N</span>{" "}
+      {/* change text color to Indian flag color indiatext */}
+      <span className="text-7xl uppercase font-bold indiatext">D</span>{" "}
+      {/* change text color to Indian flag color indiatext */}
+      <span className="text-7xl uppercase font-bold indiatext">I</span>{" "}
+      {/* change text color to Indian flag color indiatext */}
+      <span className="text-7xl uppercase font-bold indiatext">A</span>{" "}
+      {/* change text color to Indian flag color */}
+    </div>
+  );
 };
 
 export default Components;
